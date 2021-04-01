58bfe9a7195bff2b4753fefabdcfb98471b170a6
Sinan Aslan <45533327+aslansinan2@users.noreply.github.com>
2021-04-01 11:32:57 +0300

    Relationsship between 2 table

--- product/models.py
+++ product/models.py
@@ -15,6 +15,25 @@ class Category(models.Model):
     parent= models.ForeignKey('self',blank=True,null=True,related_name='childeren',on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at =models.DateTimeField(auto_now=True)
-
     def __str__(self):
         return  self.title
+
+class Product(models.Model):
+        STATUS = (
+            ('True', 'Evet'),
+            ('False', 'Hayır')
+        )
+        category = models.ForeignKey(Category,on_delete=models.CASCADE) #relation with Category
+        title = models.CharField(max_length=30)
+        keywords = models.CharField(max_length=255)
+        description = models.CharField(max_length=255)
+        image = models.ImageField(blank=True, upload_to='images/')
+        price = models.FloatField()
+        amount = models.IntegerField()
+        detail = models.TextField()
+        status = models.CharField(max_length=10, choices=STATUS)
+        create_at = models.DateTimeField(auto_now_add=True)
+        update_at = models.DateTimeField(auto_now=True)
+
+        def __str__(self):
+            return self.title
