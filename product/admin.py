80f1189fe3e0fe69a484d9b263752658421f5b52
Sinan Aslan <45533327+aslansinan2@users.noreply.github.com>
2021-04-30 19:30:10 +0300

    upload images clasor created

--- product/admin.py
+++ product/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from product.models import Category, Product
-
+from product.models import Category, Product, Images
+class ProductImagesInline(admin.TabularInline):
+    model = Images
+    extra = 5
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display =['title','status']
@@ -11,6 +13,11 @@ class CategoryAdmin(admin.ModelAdmin):
 class ProductAdmin(admin.ModelAdmin):
     list_display =['title','category','price','amount','status']
     list_filter = ['status','category']
+    inlines = [ProductImagesInline]
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display =['title','product','image']
 
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Product,ProductAdmin)
+admin.site.register(Images,ImagesAdmin)
--- product/models.py
+++ product/models.py
@@ -37,3 +37,9 @@ class Product(models.Model):
 
         def __str__(self):
             return self.title
+class Images(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30,blank=True)
+    image = models.ImageField(blank=True, upload_to='images/')
+    def __str__(self):
+        return  self.title
