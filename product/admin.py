743332f009d4abbffeb881cbe461976536b113a4
Sinan Aslan <45533327+aslansinan2@users.noreply.github.com>
2021-05-07 15:19:08 +0300

    upload image shown created

--- product/admin.py
+++ product/admin.py
@@ -7,17 +7,17 @@ class ProductImagesInline(admin.TabularInline):
     extra = 5
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display =['title','status']
+    list_display =['title','status','image']
     list_filter = ['status']
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display =['title','category','price','amount','status']
+    list_display =['title','category','price','amount','image','status']
     list_filter = ['status','category']
     inlines = [ProductImagesInline]
 
 class ImagesAdmin(admin.ModelAdmin):
-    list_display =['title','product','image']
-
+    list_display =['title','product','image_tag']
+    readonly_fields = ('image_tag',)
 admin.site.register(Category,CategoryAdmin)
 admin.site.register(Product,ProductAdmin)
 admin.site.register(Images,ImagesAdmin)
--- product/models.py
+++ product/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.utils.safestring import mark_safe
+
+
 class Category(models.Model):
     STATUS= (
         ('True', 'Evet'),
@@ -17,6 +20,14 @@ class Category(models.Model):
     update_at =models.DateTimeField(auto_now=True)
     def __str__(self):
         return  self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+    image_tag.short_description = 'Image'
+
+
+
 
 class Product(models.Model):
         STATUS = (
@@ -37,9 +48,21 @@ class Product(models.Model):
 
         def __str__(self):
             return self.title
+
+
+        def image_tag(self):
+            return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+        image_tag.short_description = 'Image'
 class Images(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     title = models.CharField(max_length=30,blank=True)
     image = models.ImageField(blank=True, upload_to='images/')
     def __str__(self):
         return  self.title
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+    image_tag.short_description = 'Image'
+
