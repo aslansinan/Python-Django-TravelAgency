9b50041cedc170e41f3e31935f07b39fe9f1a7a2
Sinan Aslan <45533327+aslansinan2@users.noreply.github.co>
2021-06-14 17:59:13 +0300

    anasayfada ürünleri göstermek

--- home/views.py
+++ home/views.py
@@ -10,10 +10,17 @@ def index(request):
     setting = Setting.objects.get(pk=1)
     sliderdata= Product.objects.all()[:4]
     category=Category.objects.all()
+    dayproducts=Product.objects.all()[:3]
+    lastproducts=Product.objects.all().order_by('-id')[:3]
+    randomproducts=Product.objects.all().order_by('?')[:3]
     context = {'setting': setting,
                'page':'home',
                'category':category,
-               'sliderdata':sliderdata}
+               'sliderdata':sliderdata,
+               'dayproducts': dayproducts,
+               'lastproducts': lastproducts,
+               'randomproducts': randomproducts,
+               }
     return render(request, 'index.html', context)
 def hakkimizda(request):
     setting = Setting.objects.get(pk=1)
