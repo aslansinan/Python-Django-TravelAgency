257be19324391919229d806d792e2b5e1ec0adac
Sinan Aslan <45533327+aslansinan2@users.noreply.github.com>
2021-06-17 00:30:51 +0300

    User Profile for Admin

--- home/models.py
+++ home/models.py
@@ -1,8 +1,10 @@
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 from django.forms import ModelForm, TextInput, Textarea
+from django.utils.safestring import mark_safe
 
 
 class Setting(models.Model):
@@ -66,3 +68,27 @@ class ContactForm(ModelForm):
             'emil': TextInput(attrs={'class': 'input', 'placeholder': 'Email Adress'}),
             'message': Textarea(attrs={'class': 'input', 'placeholder': 'TourMessage','rows':'5'}),
             }
+class UserProfile(models.Model):
+    user =models.OneToOneField(User,on_delete=models.CASCADE)
+    phone = models.CharField(blank=True, max_length=30)
+    address= models.CharField(blank=True,max_length=150)
+    city= models.CharField(blank=True,max_length=150)
+    country= models.CharField(blank=True,max_length=150)
+    image= models.ImageField(blank=True,upload_to='images/users/')
+    def __str__(self):
+        return self.user.username
+    def user_name(self):
+        return '['+self.user.username + '] ' +self.user.first_name + ' ' +self.user.last_name
+
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+
+    image_tag.short_description = 'Image'
+class UserProfileForm(ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = {'phone', 'address', 'city', 'country','image'}
+
+
+
+
